35701f1055253f0efc1e48f57bbde747698f3856
Erick Gonzalez <erickgonzalez2004@gmail.com>
2022-11-13 12:49:59 -0600

    Nav eve-not v2

--- app/src/main/java/ni/edu/uca/taskitty/NewEventFragment.kt
+++ app/src/main/java/ni/edu/uca/taskitty/NewEventFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ni.edu.uca.taskitty.databinding.FragmentNewEventBinding
 
 class NewEventFragment : Fragment() {
-
+    private lateinit var binding: FragmentNewEventBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
