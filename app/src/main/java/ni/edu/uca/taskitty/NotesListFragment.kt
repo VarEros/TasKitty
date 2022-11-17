22a4c122dd0d305f1bcbfffa830c31f0b82b7519
davidquintr <davidquintr97@gmail.com>
2022-11-17 17:35:31 -0600

    visual changes in all the system

--- app/src/main/java/ni/edu/uca/taskitty/NotesListFragment.kt
+++ app/src/main/java/ni/edu/uca/taskitty/NotesListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ni.edu.uca.taskitty.adapter.NoteRecycler
@@ -39,7 +40,12 @@ class NotesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         establecerAdapter()
+
+        binding.btnAddNote.setOnClickListener {
+            findNavController().navigate(R.id.newNoteFragment)
+        }
     }
 
     private fun establecerAdapter(){
