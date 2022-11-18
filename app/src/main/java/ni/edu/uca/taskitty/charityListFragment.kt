4742f41473386604b21c9b53acf83ba7c494d8bd
davidquintr <davidquintr97@gmail.com>
2022-11-17 20:42:58 -0600

    charity item finished and fragment added

---
+++ app/src/main/java/ni/edu/uca/taskitty/charityListFragment.kt
@@ -0,0 +1,27 @@
+package ni.edu.uca.taskitty
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ni.edu.uca.taskitty.databinding.FragmentCharityListBinding
+
+class charityListFragment : Fragment() {
+
+    private lateinit var binding : FragmentCharityListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCharityListBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
