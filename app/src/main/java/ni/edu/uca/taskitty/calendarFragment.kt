4f260a14f42c7ee42526fc48ce72dc5e8e95f8be
Poncka <davidponce600@gmail.com>
2022-11-20 22:54:42 -0600

    calendario

---
+++ app/src/main/java/ni/edu/uca/taskitty/calendarFragment.kt
@@ -0,0 +1,39 @@
+package com.gtappdevelopers.kotlingfgproject
+
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ni.edu.uca.taskitty.R
+
+class calendarFragment : AppCompatActivity() {
+
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_calendar)
+
+        // initializing variables of
+        // list view with their ids.
+        dateTV = findViewById(R.id.idTVDate)
+        calendarView = findViewById(R.id.calendarView)
+
+        calendarView
+            .setOnDateChangeListener(
+                OnDateChangeListener { view, year, month, dayOfMonth ->
+                    // In this Listener we are getting values
+                    // such as year, month and day of month
+                    // on below line we are creating a variable
+                    // in which we are adding all the variables in it.
+                    val Date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+
+                    // set this date in TextView for Display
+                    dateTV.setText(Date)
+                })
+
+    }
+}
