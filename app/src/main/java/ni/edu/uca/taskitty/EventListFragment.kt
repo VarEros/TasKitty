7c8adb8420f5d012fab06106c1b4ec2218f9c605
davidquintr <davidquintr97@gmail.com>
2022-11-21 19:12:20 -0600

    minimal improvements and bug fix

--- app/src/main/java/ni/edu/uca/taskitty/EventListFragment.kt
+++ app/src/main/java/ni/edu/uca/taskitty/EventListFragment.kt
@@ -94,4 +94,9 @@ class EventListFragment : Fragment() {
         eventList = mutableListOf()
     }
 
+    private fun onClickEvent(event : Event){
+        val dialog = EventViewDialog(event)
+        dialog.show(parentFragmentManager,"custom")
+    }
+
 }
